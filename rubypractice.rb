9a483afd872294bc1d94cbaf68a8a557a5d8fcb4
tchoutdara <monkeymilitia444@gmail.com>
2019-01-11 10:28:15 -0700

    menu and user input

--- rubypractice.rb
+++ rubypractice.rb
@@ -1,10 +1,15 @@
-@list = []
+@list = ["Song1", "Song2"]
 
 def menu()
     puts "****Song List****"
     puts "1) View all music"
     puts "2) Add Song"
     puts "3) Remove Song"
+    
+    user_choice = gets.to_i
+    puts user_choice
 end
+
+
 
 menu

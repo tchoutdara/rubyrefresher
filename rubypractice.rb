d1bbf1f55ec62001f135ea20a2c5aac2fc5915d6
tchoutdara <monkeymilitia444@gmail.com>
2019-01-11 10:21:21 -0700

    making into a song list

--- rubypractice.rb
+++ rubypractice.rb
@@ -1,7 +1,10 @@
-@variable = "hello"
+@list = []
 
-def method()
-    puts @variable
+def menu()
+    puts "****Song List****"
+    puts "1) View all music"
+    puts "2) Add Song"
+    puts "3) Remove Song"
 end
 
-method
+menu
